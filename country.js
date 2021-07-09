198709148d8015bedf4b62b29ec15d46b7ca2d68
hainesma <hainesma@gmail.com>
2021-07-09 00:12:02 -0400

    Added Display method

--- country.js
+++ country.js
@@ -5,14 +5,33 @@ class Country {
         this.greeting = greeting;
         this.colors = colors;
     }
+
+    display() {
+        let color1 = document.getElementById('Color1');
+        let color2 = document.getElementById('Color2');
+        let color3 = document.getElementById('Color3');
+
+        color1.style.backgroundColor = this.colors[0];
+        color2.style.backgroundColor = this.colors[1];
+        color3.style.backgroundColor = this.colors[2];
+
+        let CountryName = document.getElementById('CountryName');
+        let OfficialLanguage = document.getElementById('OfficialLanguage');
+        let HelloWorld = document.getElementById('HelloWorld');
+
+        CountryName.innerHTML = this.name;
+        OfficialLanguage.innerHTML = this.lang;
+        HelloWorld.innerHTML = this.greeting;
+    }
 }
+
 let usa = new Country("USA", "Murican", "Why Hello there world! Have some McDonalds", ["red", "white", "blue"]);
 let mexico = new Country("Mexico", "Spanish", "Hola mundo!", ["red", "white", "green"]);
 let algeria = new Country("Algeria", "Arabic and Berber", "Sup", ["blue", "yellow", "red"])
 
 
 function SwitchCountry() {
-    let input = "USA";
+    let input = 'Mexico';
     let country;
 
     if (input === "USA") {
@@ -25,5 +44,7 @@ function SwitchCountry() {
     else if (input === "Algeria") {
         country = algeria;
     }
+    country.display();
 
 }
+
